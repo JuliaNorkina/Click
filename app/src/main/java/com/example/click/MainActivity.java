691f52bf399dd8faa51dad20fa5778ca4cb2d7a0
yulia_norkina <yulia.norkina2@gmail.com>
2019-02-11 11:37:55 +0200

    create layout, set listener on button

--- app/src/main/java/com/example/click/MainActivity.java
+++ app/src/main/java/com/example/click/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.click;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvClick = findViewById(R.id.tvClick);
+        findViewById(R.id.bClick).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
