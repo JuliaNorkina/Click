c10b781647f4a9be528fc91879412e16ba14db2f
yulia_norkina <yulia.norkina2@gmail.com>
2019-02-11 13:29:26 +0200

    create class MyHandlerThread

--- app/src/main/java/com/example/click/MainActivity.java
+++ app/src/main/java/com/example/click/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.click;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvClick;
+    private MyHandlerThread handlerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tvClick = findViewById(R.id.tvClick);
         findViewById(R.id.bClick).setOnClickListener(this);
+
+        handlerThread = new MyHandlerThread("thread");
+        handlerThread.start();
+        handlerThread.prepareHandler();
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+
+    private class MyHandlerThread extends HandlerThread {
+
+        Handler handler;
+
+        public MyHandlerThread(String name) {
+            super(name);
+        }
+
+        public void postTask(Runnable task){
+            handler.post(task);
+        }
+
+        public void prepareHandler() {
+            handler = new Handler(getLooper());
+        }
     }
 }
